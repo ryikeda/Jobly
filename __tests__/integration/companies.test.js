41a1001a26fa846aa71b90f4cbfbdd56b1ad599e
rafael ubuntu <ryikeda@outlook.com>
2020-08-04 00:58:13 +0900

    added more tests to GET /companies

--- __tests__/integration/companies.test.js
+++ __tests__/integration/companies.test.js
@@ -20,6 +20,7 @@ afterAll(async () => {
   await afterAllTests();
 });
 
+// GET routes
 describe("GET /companies", () => {
   test("Gets a list of one company", async () => {
     const resp = await request(app).get("/companies");
@@ -28,4 +29,18 @@ describe("GET /companies", () => {
       TEST_DATA.currentCompany.handle
     );
   });
+  test("Gets company with search query", async () => {
+    const resp = await request(app).get("/companies?search=test");
+    expect(resp.body.companies).toHaveLength(1);
+    expect(resp.body.companies[0].handle).toEqual(
+      TEST_DATA.currentCompany.handle
+    );
+  });
+
+  test("Returns 404 if no match for search query", async () => {
+    const resp = await request(app).get("/companies?search=notacompany");
+    expect(resp.body.status).toEqual(404);
+  });
 });
+
+describe("GET /companies/:handle", () => {});
