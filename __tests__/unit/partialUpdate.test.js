46062043e1b6f76449ad996b080bb5c3b8c1db68
rafael ubuntu <ryikeda@outlook.com>
2020-08-03 16:30:34 +0900

    added test to partialUpdate()

--- __tests__/unit/partialUpdate.test.js
+++ __tests__/unit/partialUpdate.test.js
@@ -1,9 +1,17 @@
+const sqlForPartialUpdate = require("../../helpers/partialUpdate");
+
 describe("partialUpdate()", () => {
-  it("should generate a proper partial update query with just 1 field",
-      function () {
-
+  it("should generate a proper partial update query with just 1 field", () => {
+    const { query, values } = sqlForPartialUpdate(
+      "users",
+      { first_name: "Test" },
+      "username",
+      "newusername"
+    );
     // FIXME: write real tests!
-    expect(false).toEqual(true);
-
+    expect(query).toEqual(
+      "UPDATE users SET first_name=$1 WHERE username=$2 RETURNING *"
+    );
+    expect(values).toEqual(["Test", "newusername"]);
   });
 });
