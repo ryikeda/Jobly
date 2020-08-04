f10f3fdc5adc258640f67fe36a7bd990f6707876
rafael ubuntu <ryikeda@outlook.com>
2020-08-04 14:47:53 +0900

    added database schema for jobs

--- data.sql
+++ data.sql
@@ -4,6 +4,7 @@ CREATE DATABASE jobly;
 \c jobly
 
 DROP TABLE IF EXISTS companies;
+DROP TABLE IF EXISTS jobs;
 
 CREATE TABLE companies(
     handle TEXT PRIMARY KEY,
@@ -13,9 +14,23 @@ CREATE TABLE companies(
     logo_url TEXT
 );
 
+CREATE TABLE jobs(
+    id SERIAL PRIMARY KEY,
+    title TEXT NOT NULL,
+    salary FLOAT NOT NULL,
+    equity FLOAT CHECK(equity <= 1.0) NOT NULL,
+    company_handle TEXT NOT NULL REFERENCES companies ON DELETE CASCADE
+);
+
 -- SAMPLE DATA
 INSERT INTO companies 
 VALUES
 ('apple', 'apple inc', 1000,'this is the description', 'logo.com'),
 ('google', 'Google LLC', 110000,'this is the description' , 'logo.com/google');
  
+ INSERT INTO jobs
+ (title, salary, company_handle, equity)
+ VALUES
+ ('engineer', 100000, 'apple',0.01),
+ ('software engineer', 120000, 'apple',0.02),
+ ('data scientist', 200000, 'google',0.01);
