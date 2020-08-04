2ebefc69f98766db0a5e733e21489a9f5f860ecd
rafael ubuntu <ryikeda@outlook.com>
2020-08-05 01:15:06 +0900

    added tests to POST /users

--- __tests__/integration/users.test.js
+++ __tests__/integration/users.test.js
@@ -20,6 +20,7 @@ afterAll(async () => {
   await afterAllTests();
 });
 
+// GET routes
 describe("GET /users", () => {
   test("Gets a list of one user", async () => {
     const resp = await request(app).get("/users");
@@ -46,5 +47,33 @@ describe("GET /users/:username", () => {
   test("Returns 404 if no match", async () => {
     const resp = await request(app).get(`/users/notauser`);
     expect(resp.body.status).toEqual(404);
+  });
+});
+
+// POST routes
+describe("POST /users", () => {
+  test("Creates a new user", async () => {
+    const resp = await request(app).post("/users").send({
+      username: "postUser",
+      password: "password",
+      first_name: "postFirst",
+      last_name: "postLast",
+      email: "postuser@email.com",
+    });
+    expect(resp.statusCode).toBe(201);
+    const user = resp.body.user;
+    expect(user.username).not.toBe(null);
+    expect(user.fist_name).not.toBe(null);
+    expect(user.last_name).not.toBe(null);
+    expect(user.email).not.toBe(null);
+  });
+
+  test("Prevents from creating with missing username", async () => {
+    const { first_name, last_name, email } = TEST_DATA.user;
+    const password = "password";
+    const resp = await request(app)
+      .post("/users")
+      .send({ password, first_name, last_name, email });
+    expect(resp.statusCode).toBe(400);
   });
 });
